4fe090ba07bded01c2509dffa3426b6e39c420c9
grimyx <luditraktors@gmail.com>
2022-03-19 17:40:25 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>#todos</h1>
+<Tabs />
+</template>
+
+<script setup>
+import Tabs from './components/Tabs.vue';
+
+</script>
+
+<style lang="scss">
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
